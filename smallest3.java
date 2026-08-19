c57838a0ac2c83139a74bf4ca372d05e05ccb6af
lavanyasivakumar412-png <lavanyasivakumar412@gmail.com>
2026-08-19 09:52:39 +0530

    smallest one

---
+++ smallest3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class smallest3 {
+    public static void main(String[] args) {
+        
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a number 1:");
+    int num1= sc.nextInt();
+
+    System.out.println("Enter a number 2:");
+    int num2= sc.nextInt();
+
+    System.out.println("Enter a number 3:");
+    int num3= sc.nextInt();
+
+    if(num1<num2 && num1<num3){
+        System.out.println("Smallest number is "+num1);
+    }
+    else if(num2<num1 && num2<num3){
+        System.out.println("Smallest number is "+num2);
+    }
+    else{
+        System.out.println("Smallest number is "+num3);
+    }
+
+    }
+    }
+
