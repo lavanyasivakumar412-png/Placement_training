b8f5600f42f2f8cefc5d472e54f05a3390d90099
lavanyasivakumar412-png <lavanyasivakumar412@gmail.com>
2026-08-19 09:57:16 +0530

    Divide_by_5

---
+++ Divide_by_5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Divide_by_5{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num1= sc.nextInt();
+
+        if(num1%5==0){
+            System.out.println(num1+"is divisible by 5");
+        }
+        else{
+            System.out.println(num1+"is not divisible by 5");
+        }
+        
+
+    }
+    
+}
