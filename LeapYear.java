b3671929e74fc65a5f9073b4ffe53c4ac14ca48e
lavanyasivakumar412-png <lavanyasivakumar412@gmail.com>
2026-08-20 11:42:21 +0530

    changes

--- LeapYear.java
+++ LeapYear.java
@@ -7,7 +7,7 @@ public class LeapYear {
         System.out.print("Enter a year: ");
         int year = sc.nextInt();
 
-        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+        if (year % 4 == 0) {
             System.out.println("Leap Year");
         } else {
             System.out.println("Not a Leap Year");
