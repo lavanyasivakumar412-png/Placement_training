27862234a44c7eb4fef4ced4a777cd115c6ab876
lavanyasivakumar412-png <lavanyasivakumar412@gmail.com>
2026-08-20 11:18:34 +0530

    Perfect numbers

---
+++ perfect_no.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class perfect_no {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        
+        int n =sc.nextInt();
+
+        int sum = 0;
+ 
+        for(int i=0;i<n;i++){
+            if(n % i==0){
+                sum = sum+i;
+            }
+        }
+
+        if(sum==n){
+            System.out.println(n+" is the perfect number");
+        }
+        else{
+            System.out.println(n+" is not the perfect number");
+        }
+
+
+    }
+    
+}
